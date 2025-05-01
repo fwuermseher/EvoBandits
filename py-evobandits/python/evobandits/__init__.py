71f459334d86865f298b154fb007c8487ed7ed1c
Timo Kühne <22354443+tnkuehne@users.noreply.github.com>
2025-05-01 15:29:44 +0200

    fix release workflow for manylinux (#65)
    * fix workflow
    
    * add musllinux and arm
    
    * remove unused numba dependency
    
    * remove unused sklearn dependency
    
    * fix import error
    
    * undo
    
    * revert
    
    * try fix
    
    * try fix v2
    
    * try fix v3
    
    * try fix v4
    
    * try fix v5
    
    * add explaining comments
    
    * try fix v6
    
    * try fix v6.1
    
    * try fix v6.1
    
    * try fix v7
    
    * try fix v7.1
    
    * remove unused code

--- py-evobandits/python/evobandits/__init__.py
+++ py-evobandits/python/evobandits/__init__.py
@@ -1,15 +1,21 @@
+import importlib.util
+
 from evobandits import logging
 from evobandits.evobandits import EvoBandits
 from evobandits.params import CategoricalParam, FloatParam, IntParam
-from evobandits.search import EvoBanditsSearchCV
 from evobandits.study import Study
 
 __all__ = [
     "EvoBandits",
-    "EvoBanditsSearchCV",
     "logging",
     "Study",
     "CategoricalParam",
     "FloatParam",
     "IntParam",
 ]
+
+if importlib.util.find_spec("sklearn") is not None:
+    # Only import and expose EvoBanditsSearchCV if sklearn is available
+    from evobandits.search import EvoBanditsSearchCV  # noqa
+
+    __all__.append("EvoBanditsSearchCV")
