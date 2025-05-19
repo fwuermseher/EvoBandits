bd674dc82a48c1c3014e8643fc1a0ecde16a9d37
Timo Kühne <22354443+tnkuehne@users.noreply.github.com>
2025-05-19 22:44:29 +0200

    fix deprecation warning (#104)

--- evobandits/benches/evobandits_benchmark.rs
+++ evobandits/benches/evobandits_benchmark.rs
@@ -1,7 +1,22 @@
-use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
+// Copyright 2025 EvoBandits
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use evobandits::evobandits::EvoBandits;
 use rand::rng;
 use rand_distr::{Distribution, Normal};
+use std::hint::black_box;
 
 pub fn noisy_rosenbrock(x: &[i32]) -> f64 {
     let x_f64 = x[0] as f64 / 10.0;
