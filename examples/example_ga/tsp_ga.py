ef3c96ed8d7ab2caa0323f8cd921ff1436c5d637
fwuermseher <f.wuermseher@t-online.de>
2025-06-23 23:02:04 +0200

    add results for tsp.py

--- examples/example_ga/tsp_ga.py
+++ examples/example_ga/tsp_ga.py
@@ -385,6 +385,7 @@ if __name__ == "__main__":
     import json
     from pathlib import Path
     from datetime import datetime
+    from tqdm import tqdm
 
     script_dir = os.path.dirname(os.path.abspath(__file__))
     records_dir = Path(script_dir) / "records"
@@ -394,8 +395,7 @@ if __name__ == "__main__":
     # Repeatedly simulates the GA with a distinct configuration
     samples = 1000
     results = []
-
-    for i in range(1, samples + 1):
+    for i in tqdm(range(1, samples + 1), desc="Collecting GA samples:"):
         best_cost, _ = genetic_algorithm(
             population_size=500,
             elite_size=20,
@@ -411,44 +411,54 @@ if __name__ == "__main__":
     fig, axes = plt.subplots(
         1, 2, figsize=(10, 5), gridspec_kw={"width_ratios": [1, 2]}
     )
+    axes[0].grid(False)
     axes[0].boxplot(results)
     axes[0].set_ylabel("Total distance")
-    axes[0].grid(False)
 
-    axes[1].hist(results, bins=20, edgecolor="black", color=["#377eb8"], alpha=0.75)
+    axes[1].grid()
+    axes[1].hist(
+        results, 
+        bins=20, 
+        edgecolor="black", 
+        color=["#377eb8"], 
+        alpha=0.75
+    )
     axes[1].set_xlabel("Total distance")
     axes[1].set_ylabel("Frequency")
-    axes[1].grid()
 
     plt.tight_layout(rect=(0, 0, 1, 0.96))
-    plt.savefig(records_dir / "ga_results_distribution.pdf")
+    plt.savefig(records_dir / "tsp_ga_results_dist.pdf")
 
     # ---- Plot 2 ---- #
     # Running mean of simulation results
     plt.figure(figsize=(10, 5))
+    plt.grid()
 
     results_idx = np.arange(1, samples + 1)
-    plt.scatter(results_idx, results, label="Individual results", color="#377eb8", s=10)
+    plt.scatter(
+        results_idx, 
+        results, 
+        label="Individual results", 
+        color="#377eb8", 
+        s=10
+    )
 
     means = np.cumsum(results) / results_idx
     plt.plot(results_idx, means, label="Running mean", color="#e41a1c")
 
     plt.xlabel("Number of runs")
     plt.ylabel("Total distance")
-    plt.grid()
     plt.legend()
-    plt.savefig(records_dir / "tsp_ga_running_mean.pdf")
+    plt.savefig(records_dir / "tsp_ga_running_means.pdf")
 
-    # ---- Save Results as JSON ---- #
+    # ---- Save Results ---- #
     summary = {
         "header": {
             "description": "Results from repeated runs of the GA.",
-            "author": "Felix Wuermseher",
             "date": datetime.now().strftime("%Y-%m-%d"),
             "script": os.path.basename(__file__),
         },
         "sample_count": samples,
-        "run_seeds": list(range(1, samples + 1)),
         "results": results,
     }
     with open(records_dir / "tsp_summary.json", "w") as f:
